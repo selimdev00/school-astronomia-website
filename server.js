89415d95741b8afdb72b97e27836f9efb08e6b6e
Limmie <limmiecrazzy@gmail.com>
2021-04-10 00:14:34 +0500

    Working with express + ejs and removing static files

--- server.js
+++ server.js
@@ -1,16 +1,37 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+
 const fs = require("fs");
 
-const server = http.createServer(async (req, res) => {
-  if (req.url === "/") {
-    fs.readFile("./index.html", (err, html) => {
+app.use(express.static(`${__dirname}/assets`));
+app.use(express.static(`${__dirname}/node_modules`));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index", (err, html) => {
+    if (err) console.log(err);
+
+    fs.readFile("./data/planets-list.json", (err, planets) => {
       if (err) console.log(err);
 
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(html);
-      res.end();
+      planets = JSON.parse(planets);
+      res.render("index", planets);
     });
-  }
+  });
 });
 
-server.listen(3000);
+app.get("/planets/:name", (req, res) => {
+  const planetName = req.params.name;
+  const planetModelName = planetName.toLowerCase();
+  fs.readFile(`./data/contents/${planetModelName}.json`, (err, data) => {
+    if (err) console.log(err);
+
+    const planetContent = JSON.parse(data).content;
+
+    res.render("planet", { planetName, planetModelName, planetContent });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${3000}`));
